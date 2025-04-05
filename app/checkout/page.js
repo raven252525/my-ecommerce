1dffedabe32df18e61c0c511c35294ff79a8f8bf
raven252525 <maximyurkovsky@gmail.com>
2025-04-04 23:02:23 -0700

    created functionality for add and remove buttons, created inputs for info, about to add functionality using stateful vars

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -4,17 +4,60 @@ import { ProductsContext } from "@/components/ProductsContext"
 import { useContext, useEffect, useState } from "react"
 
 export default function CheckOutPage() {
-    const {selectedProducts} = useContext(ProductsContext)
-    const [productInfo, setProductInfo] = useState([])
+    const {selectedProducts, setSelectedProducts} = useContext(ProductsContext)
+    const [productInfos, setProductInfos] = useState([])
+    
+    
     useEffect(() => {
         const uniqueIds = [...new Set(selectedProducts)]
         fetch('/api/products?ids='+uniqueIds.join(','))
         .then(response => response.json())
-        .then(json => setProductInfo(json))
+        .then(json => setProductInfos(json))
     }, [selectedProducts])
+
+    function moreOfThisProduct(id) {
+        setSelectedProducts(prev => [...prev, id])
+    }
+    function lessOfThisProduct(id) {
+        const pos = selectedProducts.indexOf(id)
+        if (pos !== -1) {
+            setSelectedProducts(prev => {
+                return prev.filter((value, index) => index !== pos)})
+        }
+    }
+
     return (
         <Layout>
-            {selectedProducts.join(',')}
+            {!productInfos.length && (
+                <div>no products in your shopping cart</div>
+            )}
+            {productInfos.length && productInfos.map((productInfo, productInfoIndex) => (
+                <div key={productInfoIndex} className="flex mb-5">
+                    <div className="bg-gray-100 p-3 rounded-xl shrink-0 ">
+                        <img className="w-24" src={productInfo.picture} alt=""/>
+                    </div>
+                    <div className="pl-4">
+                        <h3 className="font-bold text-lg">{productInfo.name}</h3>
+                        <p className="text-sm leading-4 text-gray-500">{productInfo.description}</p>
+                        <div className="flex">
+                            <div className="grow">{productInfo.price}</div>
+                            <div>
+                                <button onClick={() => lessOfThisProduct(productInfo._id)} className="border border-emerald-500 px-2 rounded-lg text-emerald-500">-</button>
+                                <span className="px-2">
+                                    {selectedProducts.filter(id => id === productInfo._id).length}
+                                </span>
+                                <button onClick={() => moreOfThisProduct(productInfo._id)} className="bg-emerald-500 px-2 rounded-lg text-white">+</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <div>
+                <input className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2" type="text" placeholder="Street Address, Number"/>
+                <input className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2" type="text" placeholder="City and Postal Code"/>
+                <input className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2" type="text" placeholder="Your Name"/>
+                <input className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2" type="text" placeholder="Email Address"/>
+            </div>
         </Layout>
     )
 }
--- components/ProductC.js
+++ components/ProductC.js
@@ -16,7 +16,7 @@ export default function Product({_id, name, price, description, picture}) {
       <div className="mt-1">
       <h3 className="font-bold text-lg leading-4">{name}</h3>
       </div>
-      <p className="text-sm mt-1">{description}</p>
+      <p className="text-sm text-gray-500 mt-1">{description}</p>
       <div className="flex mt-1">
           <div className="text-2xl font-bold grow">${price}</div>
         <button onClick={addProduct} className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
